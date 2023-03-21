25118def2168578f2891a483765d69a6ac14711b
ermaitland <maitland.eleanor@gmail.com>
2023-03-21 12:51:50 +0000

    created: graph page for the graph - datas shows but bugs

---
+++ src/components/Graph.js
@@ -0,0 +1,79 @@
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function Graph({ graphData }) {
+  let dayData = [];
+  let sleepData = [];
+
+  for (let i = 0; i < graphData?.data.length; i++) {
+    dayData.push(graphData.data[i].day_logged);
+    sleepData.push(graphData.data[i].sleep);
+  }
+
+  console.log({ dayData });
+  console.log({ sleepData });
+
+  let data = {
+    labels: dayData,
+    datasets: [
+      {
+        label: `Hours of sleep`,
+        data: sleepData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  };
+
+  console.log('THE DATA', data);
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {},
+    legend: {
+      labels: {
+        fontSize: 25
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Line data={data} height={400} options={options} />
+    </div>
+  );
+}
--- src/components/User.js
+++ src/components/User.js
@@ -1,54 +1,23 @@
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto';
 import { useEffect, useState } from 'react';
 import { API } from '../lib/api';
+import Graph from './Graph';
 
 export default function User() {
-  const [graphData, setGraphData] = useState({ labels: '', dataset: [] });
+  const [graphData, setGraphData] = useState({});
 
   useEffect(() => {
     API.GET(API.ENDPOINTS.alldays, API.getHeaders())
-      .then(({ data }) => {
-        console.log(data);
-        setGraphData({
-          labels: data?.day_logged,
-          dataset: [
-            {
-              label: 'Sleep graph',
-              date: data?.sleep,
-              backgroundColor: [
-                'rgba(75,192,192,1)',
-                '#ecf0f1',
-                '#50AF95',
-                '#f3ba2f',
-                '#2a71d0'
-              ],
-              borderColor: 'black',
-              borderWidth: 2
-            }
-          ]
-        });
+      .then((data) => {
+        setGraphData(data);
       })
       .catch((e) => console.log(e));
   }, []);
 
-  // const [graphData, setGraphData] = useState({
-  //   labels: userData?.map((data) => data?.day_logged),
-  //   dataset: [
-  //     {
-  //       label: 'Sleep graph',
-  //       date: userData?.map((data) => data?.sleep),
-  //       backgroundColor: [
-  //         'rgba(75,192,192,1)',
-  //         '#ecf0f1',
-  //         '#50AF95',
-  //         '#f3ba2f',
-  //         '#2a71d0'
-  //       ],
-  //       borderColor: 'black',
-  //       borderWidth: 2
-  //     }
-  //   ]
-  // });
-  return <Line data={graphData} />;
+  console.log(graphData);
+
+  return (
+    <div>
+      <Graph graphData={graphData} />
+    </div>
+  );
 }
